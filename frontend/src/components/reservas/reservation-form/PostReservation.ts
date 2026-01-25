dd62e958d230d4866c1b3b950723d32a1404b095
Lorenzo Parra <alejandro36036@gmail.com>
2026-01-25 15:21:49 -0400

    feature: abjust of api of reservations
    abjust of api of reservations

--- frontend/src/components/reservas/reservation-form/PostReservation.ts
+++ frontend/src/components/reservas/reservation-form/PostReservation.ts
@@ -1,26 +1,25 @@
 import { getAccessToken } from "@/lib/auth";
 
 async function postReservation(value: any) {
-   
-    const token = await getAccessToken();
-    try {
-        const res = await fetch('http://localhost:3001/api/reservations', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(value),
-        });
+  const token = await getAccessToken();
+  try {
+    const res = await fetch("http://localhost:3000/api/reservations", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(value),
+    });
 
-        alert("Reservación creada con éxito");
+    alert("Reservación creada con éxito");
 
-        return res;
+    return res;
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    } catch (error) {
-        alert("Error al crear la reservación");
-          throw new Error('Error al crear la reservación');
-    }
+  } catch (error) {
+    alert("Error al crear la reservación");
+    throw new Error("Error al crear la reservación");
+  }
 }
 
-export default postReservation
+export default postReservation;
